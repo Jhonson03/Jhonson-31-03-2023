a1aa6f3c03fe5a1dce46d1fdf8c04eb7b81eadbf
Jhonson03 <ls0303032022@unab.edu.sv>
2023-03-31 23:31:11 -0600

    Add Project

---
+++ Project/DAO/CrudProductos.cs
@@ -0,0 +1,99 @@
+﻿using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAO
+{
+    public class CrudProductos
+    {
+        public void AgregarProducto(Producto ParametroProducto)
+        {
+            using(AlmacenContext db = new AlmacenContext())
+            {
+                Producto Pro = new Producto();
+
+                Pro.Nombre = ParametroProducto.Nombre;
+                Pro.Descripcion = ParametroProducto.Descripcion;
+                Pro.Precio = ParametroProducto.Precio;
+                Pro.Stock = ParametroProducto.Stock;
+                db.Add(Pro);
+                db.SaveChanges();
+            }
+        }
+
+        public Producto ProductIndivi(int id)
+        {
+            using (AlmacenContext db = new AlmacenContext())
+            {
+                var buscar = db.Productos.FirstOrDefault(x=>x.Id==id);
+                return buscar;
+            }
+        }
+
+        public void ActualizarProduct(Producto ParametroProduct, int Lector)
+        {
+            using (AlmacenContext db = new AlmacenContext())
+            {
+                var buscar = ProductIndivi(ParametroProduct.Id);
+
+                if (buscar == null)
+                {
+                    Console.Write("El id no existe");
+                }
+                else
+                {
+                    switch (Lector)
+                    {
+                        case 1:
+                            buscar.Nombre = ParametroProduct.Nombre;
+                            break;
+
+                        case 2:
+                            buscar.Descripcion = ParametroProduct.Descripcion;
+                            break;
+
+                        case 3:
+                            buscar.Precio = ParametroProduct.Precio;
+                            break;
+
+                        case 4:
+                            buscar.Stock = ParametroProduct.Stock;
+                            break;
+                    }
+                    db.Update(buscar);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public String EliminarProduct(int id)
+        {
+            using (AlmacenContext db = new AlmacenContext())
+            {
+                var buscar = ProductIndivi(id);
+
+                if (buscar == null)
+                {
+                    return "El producto no existe";
+                }
+                else
+                {
+                    db.Productos.Remove(buscar);
+                    db.SaveChanges();
+                    return "El producto se elimino";
+                }
+            }
+        }
+
+        public List<Producto> ListarProduct()
+        {
+            using (AlmacenContext db = new AlmacenContext())
+            {
+                return db.Productos.ToList();
+            }
+        }
+    }
+}
---
+++ Project/Program.cs
@@ -0,0 +1,192 @@
+﻿using Project.DAO;
+using Project.Models;
+
+CrudProductos CrudProductos = new CrudProductos();
+Producto Pro = new Producto();
+
+bool Continuar = true;
+
+while (Continuar)
+{
+
+
+
+    Console.Write(@"Menu
+Pulse 1 Para Insertar un nuevo Producto
+Pulse 2 Para Actualizar un Producto
+Pulse 3 Para Eliminar un Producto
+Pulse 4 Para Mostrar un Listado de Productos
+Pulse 5 Para Salir
+-> ");
+
+    var Menu = Convert.ToInt32(Console.ReadLine());
+
+    switch (Menu)
+    {
+        case 1:
+            bool bucle = true;
+            while (bucle == true)
+            {
+                Console.Write("Ingrese el nombre del producto -> ");
+                Pro.Nombre = Console.ReadLine();
+                Console.Write("\nIngrese una breve descripcion del producto -> ");
+                Pro.Descripcion = Console.ReadLine();
+                Console.Write("\nIngrese el precio del producto -> $");
+                Pro.Precio = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("\nIngrese la cantidad de productos existentes");
+                Pro.Stock = Convert.ToInt32(Console.ReadLine());
+
+                CrudProductos.AgregarProducto(Pro);
+
+                Console.Write(@"
+El Producto se ingreso correctamente
+
+Pulsa 1 para seguir insertando Productos
+Pulsa 2 para salir
+-> ");
+
+                var Menu2 = Convert.ToInt32(Console.ReadLine());
+
+                switch (Menu2)
+                {
+                    case 1:
+                        bucle = true;
+                        break;
+
+                    case 2:
+                        Console.Write("Gracias por utilizar el programa");
+                        bucle = false;
+                        break;
+
+                    default:
+                        Console.Write("Ingrese una opcion del menu");
+                        break;
+                }
+            }
+            break;
+
+        case 2:
+            Console.Write("Actualizar Productos");
+            Console.Write("\nIngrese el Id del producto a actualizar -> ");
+
+            var BuscarProductIndiviU = CrudProductos.ProductIndivi(Convert.ToInt32(Console.ReadLine()));
+
+            if (BuscarProductIndiviU == null)
+            {
+                Console.Write("El producto no existe");
+            }
+            else
+            {
+                Console.Write($"\nId: {BuscarProductIndiviU.Id}, Nombre: {BuscarProductIndiviU.Nombre}, Descripcion: {BuscarProductIndiviU.Descripcion}, Precio: {BuscarProductIndiviU.Precio}, Stock: {BuscarProductIndiviU.Stock}");
+                Console.Write(@"
+Ingrese 1 para actualizar el Nombre
+Ingrese 2 para actualizar la Descripcion
+Ingrese 3 para actualizar el Precio
+Ingrese 4 para actualizar el Stock
+-> ");
+
+                var Lector = Convert.ToInt32(Console.ReadLine());
+
+                switch (Lector)
+                {
+                    case 1:
+                        Console.Write("Ingrese el nombre -> ");
+                        BuscarProductIndiviU.Nombre = Console.ReadLine();
+                        break;
+
+                    case 2:
+                        Console.Write("Ingrese la descripcion -> ");
+                        BuscarProductIndiviU.Descripcion = Console.ReadLine();
+                        break;
+
+                    case 3:
+                        Console.Write("Ingrese el precio -> ");
+                        BuscarProductIndiviU.Precio = Convert.ToDecimal(Console.ReadLine());
+                        break;
+
+                    case 4:
+                        Console.Write("Ingrese el stock -> ");
+                        BuscarProductIndiviU.Stock = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    default:
+                        Console.Write("No se encuentra esta opcion en el panel de menu");
+                        break;
+                }
+
+                CrudProductos.ActualizarProduct(BuscarProductIndiviU, Lector);
+                Console.Write("Actualizacion completa");
+            }
+
+            break;
+
+        case 3:
+            Console.Write("\nIngrese el Id del producto a Eliminar");
+
+            var ProductIndiviD = CrudProductos.ProductIndivi(Convert.ToInt32(Console.ReadLine()));
+
+            if (ProductIndiviD == null)
+            {
+                Console.Write("Este producto no existe");
+            }
+            else
+            {
+                Console.Write("Eliminar Producto");
+                Console.Write($"\nId: {ProductIndiviD.Id}, Nombre: {ProductIndiviD.Nombre}, Descripcion: {ProductIndiviD.Descripcion}, Precio: {ProductIndiviD.Precio}, Stock: {ProductIndiviD.Stock}");
+
+                Console.Write("El producto encontrado es el correcto ingrese 1 -> ");
+                var Lector = Convert.ToInt32(Console.ReadLine());
+
+                if (Lector == 1)
+                {
+                    var Id = Convert.ToInt32(Console.ReadLine());
+                    Console.Write(CrudProductos.EliminarProduct(Id));
+                }
+                else
+                {
+                    Console.Write("Inicia nuevamente");
+                }
+            }
+            break;
+
+        case 4:
+            Console.Write("Lista de Productos");
+
+            var ListaProductos = CrudProductos.ListarProduct();
+
+            foreach (var i in ListaProductos)
+            {
+                Console.Write($"\nId: {i.Id}, Nombre: {i.Nombre}, Descripcion{i.Descripcion}, Precio: {i.Precio}, Stock: {i.Stock}");
+            }
+            break;
+
+        case 5:
+            Console.Write("Gracias por usar el programa, vuelva pronto");
+            Continuar = false;
+            break;
+
+        default:
+            Console.Write("La opcion no se encuentra disponible");
+            break;
+    }
+
+    Console.Write(@"
+Desea continuar?
+
+Ingrese Si para continuar usando el programa
+Ingrese No para salir
+-> ");
+
+    var cont = Console.ReadLine();
+
+    if (cont.Equals("No"))
+    {
+        Continuar = false;
+    }
+    else if (cont.Equals("Si"))
+    {
+        Continuar = true;
+    }
+}
+
+Console.Write("Gracias por usar el programa, vuelva pronto");
